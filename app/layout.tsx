0128f5b8c1b3290e7c7970bcc74163a41f16ece0
timnirmal <timnirmal@gmail.com>
2024-02-17 19:06:32 +0530

    Auth Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import {cookies} from 'next/headers';
 
 import '@/app/globals.css';
 import AuthProvider from "@/components/AuthProvider";
+import NavBar from "@/components/NavBar/NavBar";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -29,12 +30,13 @@ export default async function RootLayout({children,}: Readonly<{ children: React
     return (
         <html lang="en">
         <body>
+        <NavBar />
         <div className="flex min-h-screen flex-col items-center justify-center py-2">
             <main
                 className="flex w-full flex-1 shrink-0 flex-col items-center justify-center px-8 text-center sm:px-20">
-                <h1 className="mb-12 text-5xl font-bold sm:text-6xl">
-                    Next.js with <span className="font-black text-green-400">Supabase</span>
-                </h1>
+                {/*<h1 className="mb-12 text-5xl font-bold sm:text-6xl">*/}
+                {/*    Next.js with <span className="font-black text-green-400">Supabase</span>*/}
+                {/*</h1>*/}
                 <AuthProvider accessToken={session?.access_token}>{children}</AuthProvider>
             </main>
         </div>
